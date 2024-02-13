b0fb49dfa350e728e6a6ea676a16b8891293fa28
JasmitaVirani101 <jasmita@ambiplatforms.com>
2024-02-13 11:38:50 +0530

    add the CRUD operation for model(base)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request
+from werkzeug.utils import secure_filename
+
 import cv2
 
 import torch
@@ -271,7 +273,54 @@ def set_model_and_stream():
     thread.start()
 
     return jsonify({'message': 'Streaming started', 'rtmp_url': f"{camera_url}_{model_name}"})
+########################################################################################################
+############# Model upload ,delete,rename ###############
+#####upload
+ALLOWED_EXTENSIONS = {'pt'}
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload_model', methods=['POST'])
+def upload_model():
+    if 'model' not in request.files:
+        return jsonify({'error': 'No model file part'}), 400
+    file = request.files['model']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(MODEL_BASE_PATH, filename))
+        return jsonify({'message': 'Model uploaded successfully'}), 200
+    else:
+        return jsonify({'error': 'Invalid file type. Only .pt files are allowed'}), 400
+    
+#############rename
+@app.route('/rename_model', methods=['POST'])
+def rename_model():
+    data = request.get_json()
+    old_name = data.get('old_name')
+    new_name = data.get('new_name')
+    old_path = os.path.join(MODEL_BASE_PATH, old_name)
+    new_path = os.path.join(MODEL_BASE_PATH, new_name)
+    if not os.path.exists(old_path):
+        return jsonify({'error': 'Old model does not exist'}), 404
+    if os.path.exists(new_path):
+        return jsonify({'error': 'New model name already exists'}), 409
+    os.rename(old_path, new_path)
+    return jsonify({'message': 'Model renamed successfully'}), 200
+#########delet
+@app.route('/delete_model', methods=['POST'])
+def delete_model():
+    data = request.get_json()
+    model_name = data.get('model_name')
+    model_path = os.path.join(MODEL_BASE_PATH, model_name)
+    if not os.path.exists(model_path):
+        return jsonify({'error': 'Model does not exist'}), 404
+    os.remove(model_path)
+    return jsonify({'message': 'Model deleted successfully'}), 200
+
+##############################################################################################
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0",port=5000)
 
